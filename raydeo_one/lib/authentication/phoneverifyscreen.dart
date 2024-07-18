448664ce45de07b60fe7ae4f0e56326f9483a9f8
Prakash Dodawad <prakash@mobil80.com>
2024-07-18 15:11:30 +0530

    initial commit

--- raydeo_one/lib/authentication/phoneverifyscreen.dart
+++ raydeo_one/lib/authentication/phoneverifyscreen.dart
@@ -69,7 +69,7 @@ class _PhoneVerifyScreenState extends State<PhoneVerifyScreen> {
                         Container(
                           width: 180,
                           height: 180,
-                          child: ClipRRect(
+                          child: const ClipRRect(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(15)),
                               child: Image(
